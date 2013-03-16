2caea4193277df338fb9a0afb65e533ceaf0ac07
Josh Gillham <usajoshgillham@gmail.com>
2013-03-16 16:31:03 -0600

    New feature: showing score.

---
+++ src/adrills/GradeAll.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package adrills;
+
+/**
+ *
+ * @author jgillham
+ */
+public class GradeAll {
+    static public void main( String[] args ) {
+        int score = GradeArrayQuestions.grade();
+        int possiblePoints = GradeArrayQuestions.possiblePoints;
+        score += GradeLoopQuestions.grade();
+        possiblePoints += GradeLoopQuestions.possiblePoints;
+        System.out.printf( "You got %d out of %d.\n", score, possiblePoints );
+    }    
+}
--- src/adrills/GradeLoopQuestions.java
+++ src/adrills/GradeLoopQuestions.java
@@ -12,14 +12,24 @@ import java.io.PrintStream;
  * @author jgillham
  */
 public class GradeLoopQuestions {
+    static public int possiblePoints = 0;
     static public void main( String[] args ) {
+        int score = grade();
+        System.out.printf( "You got %d out of %d.\n", score, possiblePoints );
+    }
+    static public int grade() {
+        int result = 0;
+        possiblePoints = 0;
         try {
+            ++possiblePoints;
             test_outputCount101();
+            ++result;
             System.out.println( "test_outputCount101 successful!" );
         }
         catch ( Exception e ) {
             e.printStackTrace();
         }
+        return result;
     }
         
     static public void test_outputCount101() throws Exception {
@@ -34,7 +44,7 @@ public class GradeLoopQuestions {
 
                 if ( !actual.equals( "1\r\n" ) &&
                         !actual.equals( "1\n" ) ) {
-                    throw new Exception( "Bad answer: yours was \"" + 
+                    throw new Exception( "Bad answer: for n = 1 yours was \"" + 
                         actual + "\"" );
                 }
             }
@@ -47,7 +57,7 @@ public class GradeLoopQuestions {
 
                 if ( !actual.equals( "1\r\n2\r\n3\r\n4\r\n5\r\n" ) &&
                         !actual.equals( "1\n2\n3\n4\n5\n" ) ) {
-                    throw new Exception( "Bad answer: yours was \"" + 
+                    throw new Exception( "Bad answer: for n = 5 yours was \"" + 
                         actual + "\"" );
                 }
             }
@@ -60,7 +70,7 @@ public class GradeLoopQuestions {
 
                 if ( !actual.equals( "1\r\n2\r\n3\r\n4\r\n5\r\n6\r\n7\r\n" ) &&
                         !actual.equals( "1\n2\n3\n4\n5\n6\n7\n" ) ) {
-                    throw new Exception( "Bad answer: yours was \"" + 
+                    throw new Exception( "Bad answer: for n = 7 yours was \"" + 
                         actual + "\"" );
                 }
             }
