c1e82ad217a443267429f15d1bb15d4063cc669c
jgillham <jgillham@MSCD09YZ.academia.metro>
2013-03-16 14:55:48 -0600

    Wrote a question for "for" loops.

---
+++ src/adrills/LoopQuestions.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package adrills;
+
+/**
+ *
+ * @author jgillham
+ */
+public class LoopQuestions extends Questions {
+    /**
+     * Question #A.1)
+     * The method uses a loop (recommend a for loop) to print numbers 1 to n to
+     *  the system output. For example, for n = 5:
+     * 1
+     * 2
+     * 3
+     * 4
+     * 5
+     * 
+     * Hint: use <code>System.out.println()</code>
+     * 
+     * @param n is the highest number.
+     */
+    public static void outputCount101( int n) {
+        //TODO delete the line below and write your solution.
+        throw new UnansweredQuestionException();
+    }
+}
